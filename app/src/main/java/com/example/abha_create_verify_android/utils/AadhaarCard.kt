3ec0ea9c9a4da25aeea78d83e57a30ed0f977593
SanoferSameera <sanofersameera25@gmail.com>
2023-11-17 17:39:25 +0530

    424 | Add. to decode secured Aadhaar qr code
    Co-authored-by:yenugukeerthana <keerthana.yenugu@thoughtworks.com>

---
+++ app/src/main/java/com/example/abha_create_verify_android/utils/AadhaarCard.kt
@@ -0,0 +1,12 @@
+package com.example.abha_create_verify_android.utils
+
+class AadhaarCard {
+
+    var name = ""
+    var dateOfBirth = ""
+    var gender = ""
+    var district = ""
+    var pinCode = ""
+    var state = ""
+    var last4DigitAadhaar = ""
+}
